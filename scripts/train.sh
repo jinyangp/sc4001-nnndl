2edb62b73f629e6b32410912e91b558509698c8c
jinyangp <jinyangpeh@gmail.com>
2025-03-18 18:18:47 +0800

    implementation: bash scripts for cluster.

---
+++ scripts/train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH --partition=UGGPU-TC1
+#SBATCH --qos=normal
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1 
+#SBATCH --mem=24G 
+#SBATCH --job-name=test 
+#SBATCH --time=360
+#SBATCH --output=output_%x_%j.out 
+#SBATCH --error=error_%x_%j.err 
+  
+module load anaconda 
+source activate base # ( this would activate your conda base env )
+conda activate sc4001
+python -u train.py --name=180325-expt1 --config=configs/vit_head_tuned_v1.yaml --gpus 0 --max_epochs=100 --batch_size=16 --grad_acc=2
